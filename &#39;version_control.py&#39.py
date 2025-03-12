1f05de64da013e828551be4d930bd7b59e8d0553
24369Daniru <24369@students.lynfield.school.nz>
2025-03-13 09:33:56 +1300

    Update &#39;version_control.py&#39.py

--- &#39;version_control.py&#39.py
+++ &#39;version_control.py&#39.py
@@ -3,3 +3,9 @@ print("Hello World !")
 # Version 2
 name = input("Enter your name: ")
 print("Hello, {name}!")
+# Version 3
+while True:
+name = input("Enter your name (or 'exit' to quit): ")
+if name.lower() == 'exit':
+break
+print("Hello, {name}!")
